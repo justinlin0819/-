ed8b24670de44a778a334724cdc5a851c789bebd
Your Name <you@example.com>
2018-07-28 23:27:27 +0800

    Initial Commit

---
+++ 棒打老虎雞吃蟲/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  棒打老虎雞吃蟲
+//
+//  Created by Justin Lin on 2018/7/28.
+//  Copyright © 2018年 Justin Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
